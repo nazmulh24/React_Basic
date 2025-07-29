a1fc8f25d68ccee74a037eaa9c81d815fa335a77
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-29 13:29:26 +0600

    28.5 Showing a List of Data Using Map

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 // import "./App.css";
 import FirstComponent from "./components/FirstComponent";
+import List from "./components/List";
 
 function App() {
   return (
     <>
-      <FirstComponent />
+      {/* <FirstComponent /> */}
+
+      <List></List>
     </>
   );
 }
---
+++ src/components/List.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const List = () => {
+  const items = ["Item 1", "Item 2", "Item 3"];
+
+  items.map((item) => {
+    console.log(item);
+  });
+
+  return (
+    <div>
+      <h1>Item List</h1>
+
+      <div>
+        {items.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </div>
+      {/* Another way */}
+      <div>
+        {items.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </div>
+      {/* Another way like --> List */}
+      <ul>
+        {items.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default List;
