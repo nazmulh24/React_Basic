349a17bad77a1941cea1f27336f5132c521e3e31
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-30 04:21:31 +0600

    29.9 Fetch Data Using Axios, Handling Errors, Network Tab

--- src/components/ApiFetch.jsx
+++ src/components/ApiFetch.jsx
@@ -1,17 +1,28 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const ApiFetch = () => {
   const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
+
+  //   useEffect(() => {
+  //     fetch("https://jsonplaceholder.typicode.com/users")
+  //       .then((res) => res.json())
+  //       .then((data) => setUsers(data));
+  //     //   .then((data) => console.log(data));
+  //   }, []);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
-    //   .then((data) => console.log(data));
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((data) => setUsers(data.data))
+      .catch((err) => setError(err.message));
+    //   .catch((err) => console.log(err.message));
   }, []);
 
   return (
     <div>
+      {error && <p className="text-red-500">{error}</p>}
       <ul className="list-disc m-5">
         {users.map((user) => (
           <li key={user.id}>
