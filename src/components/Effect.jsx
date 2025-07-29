6e993e83a0eadc06c736056ea8b390c9ee840495
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-30 03:55:24 +0600

    29.7 Understanding Effect Dependancies

--- src/components/Effect.jsx
+++ src/components/Effect.jsx
@@ -1,15 +1,21 @@
-import React, { useEffect } from "react";
-
-let count = 0;
+import React, { useEffect, useState } from "react";
+import Button from "./Button";
 
 const Effect = () => {
+  const [users, setUsers] = useState("");
+  const [dependencyA, setdependencyA] = useState(0);
+  const [dependencyB, setdependencyB] = useState(0);
+
   useEffect(() => {
     console.log("Effect Occured");
-  }, []);
+    setUsers(["John Doe"]);
+  }, [dependencyA, dependencyB]);
 
   return (
     <div>
-      <h1>User List : {++count}</h1>
+      <h1>User List</h1>
+      <Button onClick={() => setdependencyA(Math.random())}>Click_A</Button>
+      <Button onClick={() => setdependencyB(Math.random())}>Click_B</Button>
     </div>
   );
 };
