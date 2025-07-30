d32edbba99412b5037906935729acf9bfb026139
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-30 19:21:26 +0600

    30.5 Form Validation

--- src/components/HookForm.jsx
+++ src/components/HookForm.jsx
@@ -2,7 +2,13 @@ import { useForm } from "react-hook-form";
 import Button from "./Button";
 
 const HookForm = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+//   console.log(errors);
 
   const onSubmit = (data) => {
     console.log(data);
@@ -17,13 +23,23 @@ const HookForm = () => {
             Name :
           </label>
           <input
-            {...register("name")}
+            {...register("name", {
+              required: true,
+              maxLength: 20,
+            })}
             id="name"
             type="text"
             className="px-3 py-2 border rounded-lg w-full bg-gray-100 text-gray-900 focus:ring-blue-500 focus:border-blue-500"
             placeholder="Enter your name here..."
-            // required
           />
+          {errors.name?.type === "required" && (
+            <span className="text-red-500 text-sm">Name is required</span>
+          )}
+          {errors.name?.type === "maxLength" && (
+            <span className="text-red-500 text-sm">
+              Name must be at most 20 characters
+            </span>
+          )}
         </div>
 
         {/* Age */}
@@ -32,13 +48,24 @@ const HookForm = () => {
             Age :
           </label>
           <input
-            {...register("age", { valueAsNumber: true })}
+            {...register("age", {
+              valueAsNumber: true,
+              required: true,
+              min: 18,
+            })}
             id="age"
             type="number"
             className="px-3 py-2 border rounded-lg w-full bg-gray-100 text-gray-900 focus:ring-blue-500 focus:border-blue-500"
             placeholder="Enter your age here..."
-            // required
           />
+          {errors.age?.type === "required" && (
+            <span className="text-red-500 text-sm">Age is required</span>
+          )}
+          {errors.age?.type === "min" && (
+            <span className="text-red-500 text-sm">
+              Age must be at least 18
+            </span>
+          )}
         </div>
 
         {/* Email */}
@@ -47,13 +74,15 @@ const HookForm = () => {
             Email :
           </label>
           <input
-            {...register("email")}
+            {...register("email", { required: true })}
             id="email"
             type="email"
             className="px-3 py-2 border rounded-lg w-full bg-gray-100 text-gray-900 focus:ring-blue-500 focus:border-blue-500"
             placeholder="Enter your email here..."
-            // required
           />
+          {errors.email && (
+            <span className="text-red-500 text-sm">Email is required</span>
+          )}
         </div>
 
         {/* Password */}
@@ -65,13 +94,15 @@ const HookForm = () => {
             Password :
           </label>
           <input
-            {...register("password")}
+            {...register("password", { required: true })}
             id="password"
             type="password"
             className="px-3 py-2 border rounded-lg w-full bg-gray-100 text-gray-900 focus:ring-blue-500 focus:border-blue-500"
             placeholder="Enter your password here..."
-            // required
           />
+          {errors.password && (
+            <span className="text-red-500 text-sm">Password is required</span>
+          )}
         </div>
 
         {/* Checkbox */}
